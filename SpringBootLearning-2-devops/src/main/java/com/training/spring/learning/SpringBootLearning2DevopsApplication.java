5c0ae3228d643e39f6a65172e4da1fd0057190ee
johnsusheel <41463916+johnsusheel@users.noreply.github.com>
2020-12-08 13:46:54 +0530

    Add files via upload

---
+++ SpringBootLearning-2-devops/src/main/java/com/training/spring/learning/SpringBootLearning2DevopsApplication.java
@@ -0,0 +1,14 @@
+package com.training.spring.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearning2DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearning2DevopsApplication.class, args);
+		System.out.println("hello Docker");
+	}
+
+}
---
+++ SpringBootLearning-2-devops/src/test/java/com/training/spring/learning/SpringBootLearning2DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.spring.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearning2DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
